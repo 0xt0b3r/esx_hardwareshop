e0ec859493bde4de00525c3671f9d0cfb7637f6b
Willgallagher22 <54562265+Willgallagher22@users.noreply.github.com>
2019-09-28 11:42:09 +0100

    Add files via upload

---
+++ esx_hardwareshop/config.lua
@@ -0,0 +1,16 @@
+Config              = {}
+Config.DrawDistance = 100
+Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
+Config.Color        = {r = 0, g = 128, b = 255}
+Config.Type         = 27
+Config.Locale       = 'en'
+
+Config.Zones = {
+
+	Hardware = {
+		Items = {},
+		Pos = {
+			{x = 44.63,   y = -1747.95,  z = 29.0}
+		}
+	}
+}
